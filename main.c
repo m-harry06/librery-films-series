02adb334878373eec2362cfc8b6d47d4577305fa
Harry <hmbengmo@gmail.com>
2024-02-08 05:40:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,19 +4,22 @@
 
 int main(){
 
+    acceuil:
+
     int user_choice_acceuil;
     int prefernce_user;
 
     menu_acceuil(); // on appelle et on execute la fonction munu acceuil pour afficher le menu
+    printf("\n");
     printf(" \t votre choix: ");
     scanf("%d", &user_choice_acceuil);
     gerer_menu_acceuil(user_choice_acceuil);
     system("cls");
-    preference_preference();
+    menu_preference();
+    printf("\n");
     printf(" \t votre choix: ");
     scanf("%d", &prefernce_user);
-    gerer_prefernce(prefernce_user);
-    system("cls");
+    gerer_preference(prefernce_user);
 
     return 0;
 }
